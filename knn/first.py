5f63881d4369b64e748541f98be2b7b0b3f1f665
yichi! <zhangyichi143@gmail.com>
2021-10-24 00:55:04 +0000

    Update knn/first.py Deleted inn/.gitkeep

---
+++ knn/first.py
@@ -0,0 +1,15 @@
+from sklearn.neighbors import KNeighborsRegressor
+
+l1 = []
+l2 = []
+for i in range(1, 1001):
+    a = []
+    a.append(i)
+    l1.append(a)
+for j in range(1, 1001):
+    b = []
+    b.append(j/2)
+    l2.append(b)
+knn: KNeighborsRegressor = KNeighborsRegressor(2)
+knn.fit(l1, l2)
+print(knn.predict([[5.642673]]))
