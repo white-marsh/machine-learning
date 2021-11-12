a0149852bd1872cbc87c1ff6d9ef711981edcbd5
star-python3 <zhangyichi143@gmail.com>
2021-11-12 22:09:07 +0800

    update

--- Linear regression algorithm/first.py
+++ Linear regression algorithm/first.py
@@ -3,16 +3,16 @@ import matplotlib.pyplot as plt
 
 l1 = []
 l2 = []
-for i in range(1, 1001):
+for i in range(1, 1001, 20):
     a = []
     a.append(i)
     l1.append(a)
-for j in range(1, 1001):
+for j in range(1, 1001, 20):
     b = []
     b.append(j/2)
     l2.append(b)
 reg = LinearRegression().fit(l1, l2)
-plt.plot(l1,l2,marker = '2')
+plt.plot(l1,l2,marker = '^')
 plt.plot(85.43232,42.71616,marker = 'o')
 plt.xlabel('num')
 plt.ylabel('num/2')
